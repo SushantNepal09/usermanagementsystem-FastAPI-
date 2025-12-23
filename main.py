34fe43408d260d9aefe64e3e17a114e384691119
Sushant Nepal <sushant.nepaloc218@gmail.com>
2025-12-23 13:42:40 +0545

    Initital commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = []
+
+@app.get("/")
+def getfunc():
+    return users
+
+@app.post("/")
+def postfunc(user: str):
+    users.append(user)
+    return users
+
+@app.put("/{key}")
+def updatefun(key:int,new_user: str):
+    if key< 0 or key >= len(users):
+        return "not found"
+    else:
+        users[key] = new_user
+        return users
+    
+    
+
+@app.delete("/{key}")
+def delfunc(key:int):
+    if key < 0 or len(users)<= key:
+        return "User not found"
+    else:
+        del_user = users[key]
+        del users[key]
+        return {"deleted_user ": del_user}
